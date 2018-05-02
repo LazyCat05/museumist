49fc034a24baa25ee6a36911f11cf6f6877c2fed
ernestocmou <ernestocamou@gmail.com>
2018-05-02 14:48:26 -0400

    updated carrier wave testing

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -40,3 +40,26 @@ feature 'user registers', %Q{
     expect(page).to_not have_content('Sign Out')
   end
 end
+
+feature "profile photo" do
+  scenario "user uploads a profile photo" do
+    visit root_path
+
+    click_link('Sign Up')
+
+    expect(page).to have_content('Sign up')
+
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Username', with: 'exampleuser'
+    fill_in 'First Name', with: 'john'
+    fill_in 'Last Name', with: 'example'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    attach_file :user_profile_photo, "#{Rails.root}/spec/support/images/photo.png"
+
+    click_button 'Sign up'
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_css("img[src*='photo.png']")
+  end
+end
