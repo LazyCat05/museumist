5a42144542573b34c2dd781fbb1729544ff159fd
Natalie Douglas <ndouglasma@gmail.com>
2018-05-03 20:18:23 -0400

    Add functionality to MuseumShow and Review React components to render half stars for reviews.  Removed console.log text.

--- app/javascript/components/Review.js
+++ app/javascript/components/Review.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 const Review = (props) => {
-  let starcomponent = []
-  let star = <i className="fas fa-star"></i>
+  let starComponent = [];
+
+  if (props.rating >= 1) {
+    const reviewAverageFraction =  props.rating - Math.trunc(props.rating);
+
     for (let i = 0; i < props.rating; i++) {
-      starcomponent.push(star)
+      starComponent.push(<i key={props.museumId.toString().concat(props.id.toString().concat(i.toString()))} className="fas fa-star"></i>);
+    }
+
+    if (reviewAverageFraction >= 0.5){
+      starComponent.push(<i key={props.museumId.toString().concat(props.id.toString().concat((starComponent.length).toString()))} className="fas fa-star-half"></i>)
+    }
+  }
+  else if (props.rating >= 0.5) {
+    starComponent.push(<i key={props.museumId.toString().concat(props.id.toString().concat((starComponent.length).toString()))} className="fas fa-star-half"></i>)
   }
 
   return(
     <div>
       <p>Review by: {props.user}</p>
-      <p>Rating: {starcomponent}</p>
+      <p>Rating: {starComponent}</p>
       <p>{props.text}</p>
     </div>
   )
